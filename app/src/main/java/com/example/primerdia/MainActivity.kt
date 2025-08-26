3422782b3e40a869f5239df1791c4d473743685a
noc3tis <adrain150604@gmail.com>
2025-08-26 13:40:51 -0600

    Se agregó comentario en el main, se agregó número de control.

--- app/src/main/java/com/example/primerdia/MainActivity.kt
+++ app/src/main/java/com/example/primerdia/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PrimerDiaTheme {
                 //Adrian Rios Ramos
+                //22130518
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
