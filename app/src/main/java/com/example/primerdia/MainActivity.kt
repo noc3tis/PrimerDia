ffa950c7282d5646b6aa673700fb3e27e4fbc9ba
noc3tis <adrain150604@gmail.com>
2025-08-27 13:31:24 -0600

    Se creó esta rama con un comentario con el nombre.

--- app/src/main/java/com/example/primerdia/MainActivity.kt
+++ app/src/main/java/com/example/primerdia/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            //Rama con el nombre
             PrimerDiaTheme {
                 //Adrian Rios Ramos
                 //22130518
